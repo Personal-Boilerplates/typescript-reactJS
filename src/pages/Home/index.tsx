3f6fedebd312c9749812cdc96a177c79f081805e
Rafael Rocha <rrocha.rafael@gmail.com>
2021-06-05 14:18:17 -0300

    created

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container className="row">
+      <h1 className="col s12 m12 l12">Home</h1>
+    </Container>
+  );
+};
+
+export default Home;
